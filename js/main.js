a8143cb12df812a2fa92a8b52412e32a03462136
yasindu1997 <yasindusathsara@gmail.com>
2023-06-18 20:38:27 +0530

    some theories done

--- js/main.js
+++ js/main.js
@@ -1,254 +1,114 @@
-console.log("Hello Yasindu's JS lession....");
-
-var email = "yasindu@gmail.com";
-console.log(email)
-
-var student_name = 90;
-
-var mark = 45.56;
-
-var grade = 'A';
-
-var isMarried = true;
+var stdName = "Yasindu" //------> String
+var age = 26; //34,56.7 //-------> number
+var isMarreid = true //boolean
 
 
-var x = 12;
-var y = 13;
+//variables ---> var, let, const
 
-var answer = x + y; //+,-,*,/,%
-console.log(answer);
+var val0 = 12;
+var val0 = 45;
 
-//single line comment
+/*let val1 = 34;
+let val1 = 90;*/
 
-/*Hi this is a 
-multiline comment
-and you are a hero*/
+/*const val3 = 34;
+const val3=78;*/
 
-//function 
-function add() {
-    var firstNumber = 10;
-    var secondNumber = 8;
+///////////////////////
 
-    alert("Answer is " + (firstNumber + secondNumber));
-    console.log("Answer is " + (firstNumber + secondNumber));
+var val4 = 45;
+val4 = 90;
+
+let val5 = 65;
+val5 = 34;
+
+/*const val6 = 89;
+val6=12;*/
+
+///////////////////////
+
+//var is globle scope
+
+var val7 = 23;
+
+{
+    console.log(val7);
+    var val8 = 56;
 }
 
-//function calling statment
-// add();
+console.log(val8);
 
-function loginUser() {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+//let is block scope
 
-    console.log(email + " " + password);
+/*let val9 = 12;
 
-    document.getElementById('answer').innerHTML = email + " " + password;
+{
+    console.log(val9);
+    let val10 = 90;
 }
 
-var age = 90;
+console.log(val10);*/
 
-var address = "Panadura"
+//const is block scope
 
+/*const val11 = 12;
 
-//flow contollers ----> if/else / elseif / switch
-
-
-//true,false, a statement that genarates true or false
-
-var age = 16;
-
-if (age == 18) {
-    console.log("Enter")
-} else if (age == 16) {
-    console.log("Call Parents");
-} else if (age == 14) {
-    console.log("call police");
-}
-else {
-    console.log("Helloooooooooooooo....")
+{
+    console.log(val11);
+    const val12 = 90;
 }
 
-//18> -----> home
-//username, password ----> home page
-//user ---> married ---> display x
+console.log(val12);*/
 
-function getSum() {
-    var num1 = parseInt(document.getElementById("num1").value);
-    var num2 = parseInt(document.getElementById("num2").value);
+/////////////////////////////
 
-    alert(num1 + num2);
-}
-
-//A family has three children
-//get each child's weight from user's input
-//find average
-//if avg.weight is greater than 55 alert no worries
-
-var val1 = 12;
-var val2 = 5;
-
-console.log(val1 % val2);
-
-//>, <, >=, <=, == ,!=  ------> Relational operators
-
-console.log(val1 > 17 ? "Hiii" : "Hello"); //---->ternary
-
-//&&, || ----> logical operators
-
-if (val2 > 4 || val1 == 10) {
+function saveData() {
 
 }
 
-var dbUserName = "admin@gmail.com";
-var password = "admin@123"
+saveData();
 
-var seen = 7;
+////////////////////////////
 
-//control variable
-switch (seen) {
-    case 5:
-        console.log("5 selected...");
-        break;
-    case 4:
-        console.log("4 selected....");
-        break;
-    case 7:
-        console.log("7 selected....");
-        break;
-    default:
-        console.log("Not Matched");
+
+function deleteData(name) {
+    console.log("Hiiii " + name);
 }
 
-//user input month name ----> January, February, March,....
+deleteData("Kasun");
 
-//alert ----> Duruthu, Nawam, Madin....
+function login(email, password) {
 
-////////////////////////
-
-var val3 = 89;
-
-val3 += 5; //*=,/=,-=
-
-//val3 = val3 + 5;
-
-console.log(val3);
-
-/////////////////////////
-
-var val4 = 9;
-
-//val4 = val4+1;
-
-//val4+=1;
-
-val4++; //increamnt ----> val4-- -------->decrement
-
-console.log(val4);
-
-//loops ----> for loop, while loop, do-while loop, for-each loops
-
-console.log("Hello For-Loop");
-console.log("Hello For-Loop");
-console.log("Hello For-Loop");
-console.log("Hello For-Loop");
-console.log("Hello For-Loop");
-console.log("Hello For-Loop");
-console.log("Hello For-Loop");
-console.log("Hello For-Loop");
-
-
-for (var i = 0; i < 5; i++) {
-    console.log("Hello For-Loop");
 }
+
+login("yasindu@gmial.com", "yasindu123")
 
 ///////////////////////////////
 
-var myVal = 4;
-
-//6 rounds
-for (var i = 0; i < 3; i++) {
-    if (myVal % 2 == 0) {
-        myVal += 3;
-    } else {
-        myVal += 2;
-    }
+function sumData(num1, num2) {
+    return num1 * num2;
 }
 
-console.log(myVal);
+var result = sumData(5, 4);
+console.log(result);
 
-//////////////////////////////
+/////////////////////////////
 
-var dbUserName = "admin@gmail.com";
-var dbPassword = "admin@123";
+const updateData = function () {
 
-function clearInputs() {
-    document.getElementById('email').value = "";
-    document.getElementById('password').value = "";
 }
 
-//a user can enter his username and password for three times.
-//if user entered username and password correctly alert 'login success'.
-//if not alert 'login failed' clear text inputs and give another chance to enter username and password.
-//when finshed 3 chances alert 'your account is locked'
+updateData();
 
-var rounds = 0;
+//arrow function ---> usage
 
-function onLogin() {
-    var userName = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+const loadData = () => {
 
-    if (userName == dbUserName && password == dbPassword) {
-        alert("Login Success !");
-    } else {
-        alert("Login Failed !");
-        clearInputs();
-        rounds++;
-        if (rounds == 3) {
-            alert("Your account locked !");
-        }
-    }
 }
 
-//while loop
+loadData();
 
-//true,false,statement
 
-var myNum = 20;
 
-while(myNum>15){
-    console.log("Helloooooo");
-    myNum--;
 
-    if(true){
-        //break;
-    }
-}
-
-///array//////////////////////
-
-var studentMark = 89.6;
-
-var studentMarks = [34.5, 56.5, 78, 67.4, 34, 65]; //--> index starts with 0 ends with length-1 ---> 1024
-
-console.log(studentMarks[4]);
-
-//////////////////////////////
-
-// 0---->5(4)
-// 0---->8(7)
-
-for(var i=0; i<6; i++){
-    console.log("Value is "+studentMarks[i])
-}
-
-//mx value and min value in studentMarks array
-
-//get user's name from user ---> Yasindu
-//display charactors of that name like [Y,A,S,I,N,D,U]
-
-var balance = 80;
-
-do{
-    console.log("running.......");
-}while(balance>=56);
 
